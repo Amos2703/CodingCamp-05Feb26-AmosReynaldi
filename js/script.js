b832a6028b068c50228bfda8d8896820a6c87ccb
Amos2703 <reynaldiamos@gmail.com>
2026-02-07 11:51:15 +0700

    End

---
+++ js/script.js
@@ -0,0 +1,16 @@
+
+function addtodo() {
+    const todoinput = document.getElementById('Todo-input');
+    const tododate = document.getElementById('Todo-date');
+    const todolist = document.getElementById('Todo-list');
+    
+    console.log('Todo:', todoinput.value, 'date:', tododate.value);
+}
+
+function deleteAllTodos() {
+
+}
+
+function filterTodos() {
+
+}
